26e1955bfa6db0e1c005f7010b924d0ce7c34cf3
Mark Soderquist <msoderquist@mastercontrol.com>
2026-06-09 14:35:33 -0600

    Update draw.io mouse and gesture mapping

--- source/main/java/com/acromere/cartesia/command/MouseAndGestureMap.java
+++ source/main/java/com/acromere/cartesia/command/MouseAndGestureMap.java
@@ -36,28 +36,28 @@ public enum MouseAndGestureMap {
 
 	DRAWIO {
 		void load( CommandMap map ) {
-//			map.add( "anchor", new CommandTrigger( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, CommandTrigger.Modifier.ANY ) );
-//
-//			// Selects ---------------------------------------------------------------
-//			map.add( "select-touch", new CommandTrigger( MouseEvent.MOUSE_RELEASED, MouseButton.PRIMARY ) );
-//			map.add( "select-toggle", new CommandTrigger( MouseEvent.MOUSE_RELEASED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL ) );
-//			map.add( "select-window-contain", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.MOVED ) );
-//			map.add( "select-window-intersect", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.SHIFT, CommandTrigger.Modifier.MOVED ) );
-//
-//			// Snaps -----------------------------------------------------------------
-//			map.add( "snap-auto-nearest", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.SECONDARY ) );
-//			map.add( "snap-auto-midpoint", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.MIDDLE ) );
-//
-//			// Camera 2D -------------------------------------------------------------
-//			map.add( "camera-move", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL, CommandTrigger.Modifier.MOVED ) );
-//			map.add( "camera-zoom", new CommandTrigger( ScrollEvent.SCROLL, CommandTrigger.Modifier.CONTROL ) );
-//			map.add( "camera-zoom", new CommandTrigger( ZoomEvent.ZOOM ) );
-//			map.add( "camera-spin", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL, CommandTrigger.Modifier.SHIFT, CommandTrigger.Modifier.MOVED ) );
-//
-//			// Camera 3D -------------------------------------------------------------
-//			// Camera walk (moving the camera forward and back)
-//			//map.add( new CommandEventKey( ScrollEvent.SCROLL, false, true, false, false ), "camera-walk" );
-//			//map.add( new CommandEventKey( ZoomEvent.ZOOM, false, true, false, false ), "camera-walk" );
+			map.add( "anchor", new CommandTrigger( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, CommandTrigger.Modifier.ANY ) );
+
+			// Selects ---------------------------------------------------------------
+			map.add( "select-touch", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.PRIMARY ) );
+			map.add( "select-toggle", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL ) );
+			map.add( "select-window-contain", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.MOVED ) );
+			map.add( "select-window-intersect", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.SHIFT, CommandTrigger.Modifier.MOVED ) );
+
+			// Snaps -----------------------------------------------------------------
+			map.add( "snap-auto-nearest", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.SECONDARY ) );
+			map.add( "snap-auto-midpoint", new CommandTrigger( MouseEvent.MOUSE_CLICKED, MouseButton.MIDDLE ) );
+
+			// Camera 2D -------------------------------------------------------------
+			map.add( "camera-move", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL, CommandTrigger.Modifier.MOVED ) );
+			map.add( "camera-zoom", new CommandTrigger( ScrollEvent.SCROLL, CommandTrigger.Modifier.CONTROL ) );
+			map.add( "camera-zoom", new CommandTrigger( ZoomEvent.ZOOM ) );
+			map.add( "camera-spin", new CommandTrigger( MouseEvent.DRAG_DETECTED, MouseButton.PRIMARY, CommandTrigger.Modifier.CONTROL, CommandTrigger.Modifier.SHIFT, CommandTrigger.Modifier.MOVED ) );
+
+			// Camera 3D -------------------------------------------------------------
+			// Camera walk (moving the camera forward and back)
+			//map.add( new CommandEventKey( ScrollEvent.SCROLL, false, true, false, false ), "camera-walk" );
+			//map.add( new CommandEventKey( ZoomEvent.ZOOM, false, true, false, false ), "camera-walk" );
 		}
 	};
 
